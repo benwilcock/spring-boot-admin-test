26b98f14a89aa43f82bc1baac605b5c6e60a8df5
benwilcock <benwilcock@gmail.com>
2022-05-17 15:31:05 +0100

    initial commit

---
+++ src/test/java/com/example/springbootadmintest/SpringBootAdminTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootadmintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
